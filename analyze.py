8e5501c50114cf0ed777c7ecdb60544bb94d675d
312432 <andy.wu.cyut.edu@gmail.com>
2026-01-29 22:44:57 +0800

    feat: 完善analyze.py FIXME: processAction應該還有錯，正在修正中

--- analyze.py
+++ analyze.py
@@ -4,6 +4,7 @@ import re
 FILENAME = "SimCat VS Dartrix VS MeowCaTS VS Rowlet (2025_05_03_17_29_56_3208)-(Rowlet(Win))_Win.txt"
 
 Player = [[] for _ in range(4)]
+abandonList = []
 
 PlayerBank = ''#莊家人物
 def getPlayerFromLoc(char):
@@ -14,17 +15,36 @@ def getPlayerFromLoc(char):
         return 0
     return (order[char] - order[PlayerBank]) % 4
 
-def processAction(actionStr):
-    if(actionStr.startswith('M')):#摸牌，牌河進手排
-    if(actionStr.startswith('HD')):#打牌，將手排捨 去牌池
-    if(actionStr.startswith('MD')):#摸牌，牌河進手排
-   
-    if(actionStr.startswith('MD'))
+def processAction(Step):
+    actionStr = Step[2]
+    try:
+        if(actionStr == 'M'):#摸牌，進手排
+            Player[getPlayerFromLoc(Step[1])].append(Step[3])
+        if(actionStr == 'HD'):#打牌，捨手排 去牌池
+            Player[getPlayerFromLoc(Step[1])].remove(Step[3])
+            abandonList.append(Step[3])
+        if(actionStr == 'MD'):#摸切
+            pass
+        if(actionStr == 'P'):#碰 捨對手排，進自己牌池(三排相同)
+            abandonList.remove(Step[3])
+            Player[getPlayerFromLoc(Step[1])].append(Step[3])
+        if(actionStr == 'E' or actionStr == 'EM'):#吃 捨對手排，進自己牌池(連續)
+            abandonList.remove(Step[4])
+            Player[getPlayerFromLoc(Step[1])].append(Step[4])
+            pass
+        if(actionStr == 'EL'):#???
+            pass
+        if(actionStr == 'UG'):#吃 捨對手排，進自己牌池(連續)
+            pass
+        if(actionStr == 'H'):#???
+            pass
+    except:
+        print('err')
+        pass
 
 
 Step = []
 def processFile():
-    Step = []
     with open(FILENAME, "r", encoding='utf-16-le') as f:
         for line in f:
             if re.match(r'\*\s*\d+\.', line):
@@ -39,14 +59,27 @@ def processFile():
                         k = 0
                     else:
                         k += 1
-    PlayerBank = Step[0][1]
+
+def strCard(cards):
+    typeDict = {0:'花', 1:'萬', 2:'筒', 3:'條', 4:'字'}
+    out = ""
+    for card in cards:
+        card = int(card)
+        out +=  str(str(int(card/10%10)) + typeDict[int(card/100)] + '(' + str(card%10) +') ')
+    return out
 
 if __name__ == "__main__":
     processFile()
+    PlayerBank = Step[0][1]
     inputIndex = int(input('輸入模擬的步驟:'))
     if(inputIndex > len(Step)):
         print("超出模擬步驟範圍")
         exit()
     for i in range(inputIndex):
-        PlayerIndex = getPlayerFromLoc(Step[i][1])
-        if Step[i][]
+        processAction(Step[i])
+        for j in range(4):
+            print("北:" + strCard(sorted(Player[getPlayerFromLoc('N')])) + '\n')
+            print("東:" + strCard(sorted(Player[getPlayerFromLoc('E')])) + '\n')
+            print("南:" + strCard(sorted(Player[getPlayerFromLoc('S')])) + '\n')
+            print("西:" + strCard(sorted(Player[getPlayerFromLoc('W')])) + '\n')
+            print("池:" + strCard(abandonList))
